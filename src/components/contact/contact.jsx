f00efbad11a720e557ce15928f58bbba13d515ba
estebanWolff <es.wolf2f@gmail.com>
2024-04-01 16:42:00 -0300

    fix-principal-image-fix-color

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,37 +1,34 @@
 import React from "react";
-import "../contact/contact.css";
 
 const contact = () => {
   return (
-    <div>
-      <section className="bg-gray-100 py-12 md:py-24 lg:py-32">
-        <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
-          <div className="space-y-2">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              Contact Us
-            </h2>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              We are here to help you with your photos, your social media and/or
-              your website. Get in touch with our team.
-            </p>
-          </div>
-          <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
-            <a
-              className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-              href="#"
-            >
-              Contactar
-            </a>
-            <a
-              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-              href="#"
-            >
-              Llamar
-            </a>
-          </div>
+    <section className="bg-gray-100 py-12 md:py-24 lg:py-32">
+      <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
+        <div className="space-y-2">
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-black">
+            Contact Us
+          </h2>
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-600">
+            We are here to help you with your photos, your social media and/or
+            your website. Get in touch with our team.
+          </p>
         </div>
-      </section>
-    </div>
+        <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
+          <a
+            className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+            href="#"
+          >
+            Contactar
+          </a>
+          <a
+            className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+            href="#"
+          >
+            Llamar
+          </a>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/galery/galery.jsx
+++ src/components/galery/galery.jsx
@@ -14,108 +14,106 @@ import FotoEdinson from "../../assets/image/presentacion/fotoEdinson.png";
 
 const galery = () => {
   return (
-    <>
-      <section className="w-full py-12 bg-white">
-        <div className="container grid gap-6 md:gap-8 px-4 md:px-6 max-w-6xl mx-auto text-black">
-          <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8">
+    <section className="w-full py-12 bg-white">
+      <div className="container grid gap-6 md:gap-8 px-4 md:px-6 max-w-6xl mx-auto text-black">
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8">
+          <div className="grid gap-1">
+            <h1 className="text-3xl font-bold tracking-tight">
+              Type of photographs we offer:
+            </h1>
+            <p className="text-gray-500 dark:text-gray-500">
+              Find the style of photography you are looking for:{" "}
+            </p>
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-3 gap-8">
+          <div className="grid gap-4 relative group">
+            <img
+              alt="Sport"
+              className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
+              height={600}
+              src={FutbolChoque}
+              width={450}
+            />
             <div className="grid gap-1">
-              <h1 className="text-3xl font-bold tracking-tight">
-                Type of photographs we offer:
-              </h1>
-              <p className="text-gray-500 dark:text-gray-400">
-                Find the style of photography you are looking for:{" "}
+              <h3 className="font-semibold">Sport</h3>
+              <p className="text-sm leading-none">
+                Photos of you or your team.
               </p>
             </div>
           </div>
-          <div className="grid lg:grid-cols-3 gap-8">
-            <div className="grid gap-4 relative group">
-              <img
-                alt="Sport"
-                className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
-                height={600}
-                src={FutbolChoque}
-                width={450}
-              />
-              <div className="grid gap-1">
-                <h3 className="font-semibold">Sport</h3>
-                <p className="text-sm leading-none">
-                  Photos of you or your team.
-                </p>
-              </div>
+          <div className="grid gap-4 relative group">
+            <img
+              alt="Running"
+              className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
+              height={600}
+              src={FotoManu}
+              width={450}
+            />
+            <div className="grid gap-1">
+              <h3 className="font-semibold">Portrait</h3>
+              <p className="text-sm leading-none">Customized portraits.</p>
             </div>
-            <div className="grid gap-4 relative group">
-              <img
-                alt="Running"
-                className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
-                height={600}
-                src={FotoManu}
-                width={450}
-              />
-              <div className="grid gap-1">
-                <h3 className="font-semibold">Portrait</h3>
-                <p className="text-sm leading-none">Customized portraits.</p>
-              </div>
+          </div>
+          <div className="grid gap-4 relative group">
+            <img
+              alt="Football"
+              className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
+              height={600}
+              src={FotoCanela}
+              width={450}
+            />
+            <div className="grid gap-1">
+              <h3 className="font-semibold">Animals</h3>
+              <p className="text-sm leading-none">
+                Your pet deserves to be remembered.
+              </p>
             </div>
-            <div className="grid gap-4 relative group">
-              <img
-                alt="Football"
-                className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
-                height={600}
-                src={FotoCanela}
-                width={450}
-              />
-              <div className="grid gap-1">
-                <h3 className="font-semibold">Animals</h3>
-                <p className="text-sm leading-none">
-                  Your pet deserves to be remembered.
-                </p>
-              </div>
+          </div>
+          <div className="grid gap-4 relative group">
+            <img
+              alt="Football"
+              className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
+              height={600}
+              src={FotoCiduadSantiago}
+              width={450}
+            />
+            <div className="grid gap-1">
+              <h3 className="font-semibold">Cities </h3>
+              <p className="text-sm leading-none">Photos for your project.</p>
             </div>
-            <div className="grid gap-4 relative group">
-              <img
-                alt="Football"
-                className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
-                height={600}
-                src={FotoCiduadSantiago}
-                width={450}
-              />
-              <div className="grid gap-1">
-                <h3 className="font-semibold">Cities </h3>
-                <p className="text-sm leading-none">Photos for your project.</p>
-              </div>
+          </div>
+          <div className="grid gap-4 relative group">
+            <img
+              alt="Football"
+              className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
+              height={600}
+              src={FotoParque}
+              width={450}
+            />
+            <div className="grid gap-1">
+              <h3 className="font-semibold">Natural</h3>
+              <p className="text-sm leading-none">Natural photos for you.</p>
             </div>
-            <div className="grid gap-4 relative group">
-              <img
-                alt="Football"
-                className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
-                height={600}
-                src={FotoParque}
-                width={450}
-              />
-              <div className="grid gap-1">
-                <h3 className="font-semibold">Natural</h3>
-                <p className="text-sm leading-none">Natural photos for you.</p>
-              </div>
-            </div>
-            <div className="grid gap-4 relative group">
-              <img
-                alt="Football"
-                className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
-                height={600}
-                src={FotoAutoViejo}
-                width={450}
-              />
-              <div className="grid gap-1">
-                <h3 className="font-semibold">Automobiles</h3>
-                <p className="text-sm leading-none">
-                  Photos of your companion on wheels .
-                </p>
-              </div>
+          </div>
+          <div className="grid gap-4 relative group">
+            <img
+              alt="Football"
+              className="rounded-lg object-cover w-full aspect-[3/4] group-hover:opacity-50 transition-opacity"
+              height={600}
+              src={FotoAutoViejo}
+              width={450}
+            />
+            <div className="grid gap-1">
+              <h3 className="font-semibold">Automobiles</h3>
+              <p className="text-sm leading-none">
+                Photos of your companion on wheels .
+              </p>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 export default galery;
